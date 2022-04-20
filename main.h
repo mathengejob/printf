1053958b7ec1d90beda8d1bfb35af8dd545d0576
mathengejob <jobmathenge90@gmail.com>
2022-04-19 19:37:47 -0700

    Incorporated Integers

--- main.h
+++ main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int _printf(const char *format, ...);
+int print_int(va_list arg);
 
 /**
  * struct identifierStruct- picks structure to printout
--- print_functions.c
+++ print_functions.c
@@ -34,4 +34,34 @@ for (i = 0; str[i]; i++)
 
 return (i);
 }
+/**
+ * print_int - writes the integer c to stdout
+ * @arg: argument
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_int(va_list arg)
+{
+
+unsigned int divisor = 1, i, resp, charPrinted = 0;
+int n = va_arg(arg, int);
+
+if (n < 0)
+{
+	_putchar('-');
+	charPrinted++;
+	n *= -1;
+}
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+{
+	resp = n / divisor;
+	_putchar('0' + resp);
+}
+return (charPrinted);
+}
+
 
