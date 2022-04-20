0db1dc7e5430582834c6682c2a0ca9b861a267db
mathengejob <jobmathenge90@gmail.com>
2022-04-19 19:56:32 -0700

    fixed binary incoporation

--- print_functions.c
+++ print_functions.c
@@ -64,3 +64,19 @@ for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
 return (charPrinted);
 }
 
+/**
+ * print_unsignedToBinary - prints an integer.
+ * @arg: argument
+ * Return: 0
+ */
+int print_unsignedToBinary(va_list arg)
+{
+
+unsigned int n = va_arg(arg, unsigned int);
+unsigned int printed;
+
+print_binary(n, &printed);
+print_binary(n, &printed);
+
+return (printed);
+}
