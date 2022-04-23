a6ef95dbc2ead6fb90650fae7037a7d82353109c
root <root@pc.localdomain>
2022-04-23 12:37:53 +0000

    Debugged strings

--- print_functions_derived.c
+++ print_functions_derived.c
@@ -84,32 +84,12 @@ int print_oct(va_list arg)
 	free(octa);
 	return (charPrinted);
 }
+
 /**
- * print_str - prints a string with a `s` (lower case) specifier
+ * print_STR - prints a string with a `S` (upper case) specificer
  * @arg: argument
  * Return: number of character printed
  */
-
-int print_str(va_list arg)
-{
-	int i;
-	char *str = va_arg(arg, char*);
-
-	if (str == NULL)
-		str = "(null)";
-	else if (*str == '\0')
-		return (-1);
-
-	for (i = 0; str[i]; i++)
-		_putchar(str[i]);
-
-	return (i);
-}
-/**
- *  * print_STR - prints a string with a `S` (upper case) specificer
- *   * @arg: argument
- *    * Return: number of character printed
- *     */
 
 int print_STR(va_list arg)
 {
